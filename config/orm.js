c351942467eb4f3ebcd908c45e5b625c24c1feab
skondilya <skondilya@gmail.com>
2017-05-04 17:50:16 -0500

    adding the new changes made

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,30 @@
 var connection = require("./connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+// Object Relational Mapper (ORM)
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
